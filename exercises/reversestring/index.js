7f6ff961e820d4ff2c73c97b2c309658360e3856
Islam Sayed <islam.sayed8@gmail.com>
2019-11-03 12:30:37 +0200

    finish reversestring problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/* function reverse(str) {
+    return str.split("").reverse().join(""); 
+}  */
+/* function reverse(str) {
+    const arr = str.split("")
+    let reversedArr = []
+    for(i=arr.length-1; i>=0; i--) {
+        reversedArr.push(arr[i])
+    } 
+    return reversedArr.join("");
+} */
+
+/* function reverse(str) {
+    let reversedStr = '';
+    for (const character of str) {
+        reversedStr = character + reversedStr;
+    }
+    return reversedStr;
+} */
+
+function reverse(str) {
+    return str.split("").reduce((rev, char) => char + rev, "")
+}
+
+
+
 
 module.exports = reverse;
