2a0b1ef958e36b64a884323abfdb431321b4b77a
Islam Sayed <islam.sayed8@gmail.com>
2019-11-03 12:16:13 +0200

    finish anagram problem

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
+}
+
+
+function cleanString(str) {
+    const cleanedStr = str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+    return cleanedStr;
+}
+
+/* function anagrams(stringA, stringB) {
+    const charMapA = buildCharMap(stringA);
+    const charMapB = buildCharMap(stringB);
+
+    if(Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+        return false;
+    }
+    for (const char in charMapA) {
+        if (charMapA[char] !== charMapB[char]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+function buildCharMap(str) {
+    let charMap = {};
+    const cleanedStr = str.replace(/[^\w]/g, "").toLowerCase()
+    for (const char of cleanedStr) {
+        charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+    }
+    return charMap;
+} */
 
 module.exports = anagrams;
