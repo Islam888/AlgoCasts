bc712c23060254de2fce7c5651c4cbbb6c6c5bf0
Islam Sayed <islam.sayed8@gmail.com>
2019-11-03 12:18:30 +0200

    finish chunk problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//function chunk(array, size) {
+//    let chunked = [];
+//    for (const elem of array) {
+//        const lastChunk = chunked[chunked.length - 1]
+//        if (!lastChunk || lastChunk.length === size) {
+//            chunked.push([elem])
+//        } else {
+//            last.push(elem)
+//        } 
+//    }
+//    return chunked;
+//} 
+function chunk(array, size) {
+    let chunked = [];
+    for (var i = 0; i < array.length; i+= size) {
+        chunked.push(array.slice(i, i+size))
+    }
+    return chunked;
+}
 
 module.exports = chunk;
