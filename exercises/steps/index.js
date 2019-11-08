7e1d6ddb50cf883ce5604410670c98c36a545924
Islam Sayed <islam.sayed8@gmail.com>
2019-11-08 08:24:44 +0200

    add recursion solution to steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/* function steps(n) {
+    let steps = ''   
+    for (var i = n; i > 0; i--) {
+        let spaces = ''
+        for (var s = i-1; s > 0 ; s--) {
+            spaces += ' '
+        }
+        
+        steps += '#'
+        console.log(steps + spaces)
+    }
+} */
+
+/* function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let stairs = '';
+        for (let column = 0; column < n; column ++) {
+            if (column <= row) {
+                stairs += '#';
+            } else {
+                stairs += ' ';
+            }
+        }
+        console.log(stairs)
+    }
+} */
+
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+    if (stair.length === n) {
+        console.log(stair);
+        steps(n, row + 1);
+        return;
+    }
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair)
+}
 
 module.exports = steps;
