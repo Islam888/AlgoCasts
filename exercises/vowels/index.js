bc698eac8d3e258b9fabd1e02aceec0ed93c7956
Islam Sayed <islam.sayed8@gmail.com>
2019-11-18 14:13:32 +0200

    finish voweles problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+/* function vowels(str) {
+    const vowelsArr = ['a', 'e', 'i', 'o', 'u'];
+    return str.toLowerCase().split("").filter(char => vowelsArr.includes(char)).length
+} */
+/* function vowels(str) {
+    const vowelsArr = ['a', 'e', 'i', 'o', 'u'];
+    let counter = 0;
+    for (const char of str.toLowerCase()) {
+        if (vowelsArr.includes(char)) {
+            counter ++
+        }
+    }
+    return counter;
+} */
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
